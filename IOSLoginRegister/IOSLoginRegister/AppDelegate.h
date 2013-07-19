de4af8d73501f9e0c097f079bc8c34359a7cf351
xujiangtao <xujiangtao@live.com>
2013-07-19 11:38:23 +0800

    初始化提交

---
+++ IOSLoginRegister/IOSLoginRegister/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IOSLoginRegister
+//
+//  Created by xujiangtao on 13-7-19.
+//  Copyright (c) 2013年 xujiangtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IOSLoginRegister/IOSLoginRegister/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IOSLoginRegister
+//
+//  Created by xujiangtao on 13-7-19.
+//  Copyright (c) 2013年 xujiangtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
