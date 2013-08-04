ba6ad4a802d4b40711baf356b9b615bc0736b02e
xujiangtao <xujiangtao@live.com>
2013-08-04 16:30:36 +0800

    添加初始类

---
+++ IOSLoginRegister/IOSLoginRegister/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  IOSLoginRegister
+//
+//  Created by xujiangtao on 13-8-4.
+//  Copyright (c) 2013年 xujiangtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
---
+++ IOSLoginRegister/IOSLoginRegister/RootViewController.m
@@ -0,0 +1,38 @@
+//
+//  RootViewController.m
+//  IOSLoginRegister
+//
+//  Created by xujiangtao on 13-8-4.
+//  Copyright (c) 2013年 xujiangtao. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
